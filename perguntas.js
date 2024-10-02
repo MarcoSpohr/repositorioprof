5c4652fe5ea7e116bcfd5441ea74c2fbcd24bca6
Marco愛 <108358565+MarcoSpohr@users.noreply.github.com>
2024-10-02 19:44:14 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,73 @@
+
+
 criaCartao(
-    'Programação',
-    'O que é um algoritmo?',
-    ' é uma sequência de raciocínios, instruções ou operações para alcançar um objetivo, sendo necessário que os passos sejam finitos e operados sistematicamente.'
+    'música',
+    'O que é o conceito de música',
+    ' é a arte de coordenar e transmitir efeitos sonoros, harmoniosos e esteticamente válidos, podendo ser transmitida através da voz ou de instrumentos musicais'
 )
 
 criaCartao(
-    'Programçao',
-    'O que a programação ensina?',
-    'escrever código para realizar tarefas específicas.'
+    'música',
+    'O que é arte da música?',
+    ' é um tipo de arte que trabalha com a harmonia entre os sons, o ritmo, a melodia, a voz.'
+
 )
 
 criaCartao(
-    'Programação',
-    'Quem criou a programação?',
-    'A primeira linguagem de programação foi criada por Ada Lovelace em 1843.'
+    'música',
+    'Como pode ser definida a música?',
+    '  a combinação de sons ao longo do tempo.'
+
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'como se diz programação em inglês',
-    'schedule'
+    'música',
+    'O que a música representa?',
+    'A música é reconhecida por muitos pesquisadores como uma modalidade que desenvolve a mente humana, promove o equilíbrio.'
+
 )
+
+criaCartao(
+    'música',
+    'Qual é a origem da música?',
+    ' provavelmente como conseqüência da observação dos sons da natureza.'
+
+)
+
+criaCartao(
+    'música',
+    'O que forma a música?',
+    ' melodia, harmonia e ritmo.'
+
+)
+
+criaCartao(
+    'música',
+    'Quais são os 7 elementos da música?',
+    ' Quais são os 7 elementos da música?.'
+
+)
+
+criaCartao(
+    'música',
+    'O que a música pode transmitir?',
+    '  Ela pode ressoar nossas experiências e valores, garantindo uma conexão emocional.'
+
+)
+
+criaCartao(
+    'música',
+    'Como surgiu a música resumo?',
+    ' A música surgiu quando o homem descobriu que, batendo um objeto no outro, ele produzia sons e que isso não era simplesmente, um tanto de barulhos.'
+
+)
+
+criaCartao(
+    'música',
+    'O que a música nos ensina?',
+    ' Ela pode ajudar a promover a empatia, a compaixão e a tolerância.'
+
+)
+
+
 
